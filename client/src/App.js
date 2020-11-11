30daf5e3562ee1e09e1d83496b83b138ab858be6
Simply-divine <hardikvu0204@gmail.com>
2020-11-11 10:20:39 +0530

    node_env production static build

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ function App() {
   };
   const webUrl =
     process.env.NODE_ENV === 'production'
-      ? 'http://www.hvu.xyz'
+      ? 'https://hvu.xyz'
       : 'http://localhost:1337';
   const handleSubmit = async (e) => {
     e.preventDefault();
